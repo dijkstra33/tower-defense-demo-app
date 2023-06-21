49f3f010ca2e961849d62ea9fde957b80ec26734
dijkstra33 <omgosu@rambler.ru>
2023-06-21 22:35:09 +0300

    Add random focus fire target selector (5th type of tower weapon)

--- TowerDefence/Assets/Scripts/Game/Weapons/TargetSelection/ClosestUnitFocusFireTargetSelector.cs
+++ TowerDefence/Assets/Scripts/Game/Weapons/TargetSelection/ClosestUnitFocusFireTargetSelector.cs
@@ -5,7 +5,7 @@ namespace Game.Weapons.TargetSelection
 {
     public class ClosestUnitFocusFireTargetSelector : ClosestUnitTargetSelector
     {
-        private TargetInfo? target;
+        private TargetInfo? currentTarget;
 
         private void Start()
         {
@@ -14,23 +14,23 @@ namespace Game.Weapons.TargetSelection
         
         private void HandleUnitDied(Unit unit)
         {
-            if (target.HasValue && target.Value.Transform == unit.transform)
+            if (currentTarget.HasValue && currentTarget.Value.Transform == unit.transform)
             {
-                target = null;
+                currentTarget = null;
             }
         }
 
         public override TargetInfo[] SelectTargets(Vector3 selectorPosition, float attackRange)
         {
-            if (target.HasValue && target.Value.Transform.gameObject.activeInHierarchy)
+            if (currentTarget.HasValue && currentTarget.Value.Transform.gameObject.activeInHierarchy)
             {
-                return new [] { target.Value };
+                return new [] { currentTarget.Value };
             }
 
             var targets = base.SelectTargets(selectorPosition, attackRange);
             if (targets != null && targets.Length > 0)
             {
-                target = targets[0];
+                currentTarget = targets[0];
             }
 
             return targets;
---
+++ TowerDefence/Assets/Scripts/Game/Weapons/TargetSelection/RandomUnitFocusFireTargetSelector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Game.HealthSystem;
+using UnityEngine;
+
+namespace Game.Weapons.TargetSelection
+{
+    public class RandomUnitFocusFireTargetSelector : TargetSelector
+    {
+        private TargetInfo? currentTarget;
+        
+        private System.Random random = new();
+
+        private void Start()
+        {
+            DeathManager.Instance.OnUnitDeath += HandleUnitDied;
+        }
+        
+        private void HandleUnitDied(Unit unit)
+        {
+            if (currentTarget.HasValue && currentTarget.Value.Transform == unit.transform)
+            {
+                currentTarget = null;
+            }
+        }
+
+        public override TargetInfo[] SelectTargets(Vector3 selectorPosition, float attackRange)
+        {
+            if (currentTarget.HasValue && currentTarget.Value.Transform.gameObject.activeInHierarchy)
+            {
+                return new [] { currentTarget.Value };
+            }
+
+            var units = FindObjectsOfType<Unit>();
+            var potentialTargets = new List<Unit>();
+            
+            foreach (var unit in units)
+            {
+                var distance = Vector3.Distance(unit.Transform.position, selectorPosition);
+                if (!unit.gameObject.activeInHierarchy || distance > attackRange)
+                {
+                    continue;
+                }
+
+                potentialTargets.Add(unit);
+            }
+            
+            if (potentialTargets.Count == 0)
+            {
+                return null;
+            }
+
+            var randomIndex = random.Next(0, potentialTargets.Count - 1);
+            var targetUnit = potentialTargets[randomIndex];
+            var targetInfo = new TargetInfo(targetUnit.GetComponent<Health>(), targetUnit.Transform);
+            currentTarget = targetInfo;
+
+            return new[] { targetInfo };
+        }
+    }
+}
